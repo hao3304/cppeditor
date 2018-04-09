be48f7d14f2290127117e4c3ab5ce193d725ca7c
jack <hao3304@qq.com>
2018-04-09 11:28:09 +0800

    init

--- server/src/tool/Compile.js
+++ server/src/tool/Compile.js
@@ -8,8 +8,8 @@ exports.compileCpp =async function (code) {
   var filename = cuid.slug();
   const rep = await writeFile(code, filename, _path)
 
-  // let commmand = 'gcc ' + _path +'/'+ filename +'.cpp -o '+ _path +'/'+ filename +'.out -lstdc++' ;
-  let commmand = 'docker exec -i cpp gcc '  +'/tmp/'+ filename +'.cpp -o /tmp/'+ filename +'.out -lstdc++' ;
+  let commmand = 'fakechroot gcc ' + _path +'/'+ filename +'.cpp -o '+ _path +'/'+ filename +'.out -lstdc++' ;
+  // let commmand = 'docker exec -i cpp gcc '  +'/tmp/'+ filename +'.cpp -o /tmp/'+ filename +'.out -lstdc++' ;
   return new Promise((resolve, reject) => {
     exec(commmand, function ( error , stdout , stderr) {
       if(error) {
