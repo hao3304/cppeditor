1133502948a1ff3ad86681d68e4a45f5b3233d87
jack <hao3304@qq.com>
2018-04-26 15:17:34 +0800

    init

--- server/src/tool/Compile.js
+++ server/src/tool/Compile.js
@@ -8,7 +8,7 @@ exports.compileCpp =async function (code) {
   // _path = path.resolve(think.ROOT_PATH,'./temp/');
   var filename = cuid.slug();
   const rep = await writeFile(code, filename, _path)
-  let commmand = 'fakechroot /usr/sbin/chroot /tmp/xenial/ gcc /tmp/'+ filename +'.cpp -o /tmp/'+ filename +'.out -lstdc++' ;
+  let commmand = 'nsjail --chroot  /tmp/xenial/ gcc /tmp/'+ filename +'.cpp -o /tmp/'+ filename +'.out -lstdc++' ;
   // let commmand = 'gcc ' + _path +'/'+ filename +'.cpp -o '+ _path +'/'+ filename +'.out -lstdc++' ;
   return new Promise((resolve, reject) => {
     exec(commmand, function ( error , stdout , stderr) {
